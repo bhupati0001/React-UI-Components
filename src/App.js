48d3b5522cab5d1bf14f12d2bbe8a7aa2ef53f83
bmohanta0001 <bmohanta0001@gmail.com>
2024-07-01 22:29:59 +0530

    things made ready for deployment on gh-pages

--- src/App.js
+++ src/App.js
@@ -4,18 +4,16 @@ import ImageSlider from './components/ImageSlider/ImageSlider';
 import RandomColor from './components/Random-color-generator/Random-color';
 import Menu from './components/RecursiveMenu/Menu';
 import StarRating from './components/StarRating/StarRating';
-import Test from './components/Test/Test';
 import LoadMore from './components/load-more-data/LoadMore';
 import { menuData } from './components/RecursiveMenu/menuData';
 import { ThemeProvider, ThemeSwitcher } from './contexts/themeContext/ThemContextProvider'
 import QRCodeGenerator from './components/QR/QRCodeGenerator';
 
 
-
 function App() {
   return (
     <div className="App">
-      {/* <Accordian />
+      <Accordian />
       <RandomColor />
       <StarRating noOfStars={10} />
       <ImageSlider />
@@ -25,9 +23,8 @@ function App() {
         <div>
           <ThemeSwitcher />
         </div>
-      </ThemeProvider> */}
+      </ThemeProvider>
       <QRCodeGenerator />
-      {/* <Test /> */}
     </div>
   );
 }
